eabd3e76fa2a4755f125bfe779a95f049b4e02ce
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-09-20 15:17:01 -0500

    w01 analyze performance

--- week01/analyze/Sorting.cs
+++ week01/analyze/Sorting.cs
@@ -1,14 +1,14 @@
 ﻿public static class Sorting {
-    public static void Run() {
-        var numbers = new[] { 3, 2, 1, 6, 4, 9, 8 };
-        SortArray(numbers);
-        Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //int[]{1, 2, 3, 4, 6, 8, 9}
+    public static void Run() { //method named run
+        var numbers = new[] { 3, 2, 1, 6, 4, 9, 8 };//sort them from lowest to highest
+        SortArray(numbers); //sorting the var nums from lowest to highest
+        Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //printing the new order which is int[]{1, 2, 3, 4, 6, 8, 9} 
     }
 
-    private static void SortArray(int[] data) {
-        for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--) {
-            for (var swapPos = 0; swapPos < sortPos; ++swapPos) {
-                if (data[swapPos] > data[swapPos + 1]) {
+    private static void SortArray(int[] data) { //Big O performance is O(n^2)
+        for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--) { //outer loop - this loop runs n times - n is the lenght of the array - runs from the end to beginning - it helps keep track of where you are in sorting
+            for (var swapPos = 0; swapPos < sortPos; ++swapPos) { //inner loop - runs fewer times as sortPos decreases
+                if (data[swapPos] > data[swapPos + 1]) { // if one num is higher than the next one it swaps their place to sort them properly
                     (data[swapPos + 1], data[swapPos]) = (data[swapPos], data[swapPos + 1]);
                 }
             }
--- week01/analyze/StandardDeviation.cs
+++ week01/analyze/StandardDeviation.cs
@@ -4,9 +4,9 @@
 /// is defined as the square root of the variance.  The variance is 
 /// defined as the average of the squared differences from the mean.
 /// </summary>
-public static class StandardDeviation {
+public static class StandardDeviation { //class sd and method run
     public static void Run() {
-        var numbers = new[] { 600, 470, 170, 430, 300 };
+        var numbers = new[] { 600, 470, 170, 430, 300 }; //creating a list of #s
         Console.WriteLine(StandardDeviation1(numbers)); // Should be 147.322 
         Console.WriteLine(StandardDeviation2(numbers)); // Should be 147.322 
         Console.WriteLine(StandardDeviation3(numbers)); // Should be 147.322 
@@ -15,23 +15,23 @@ public static class StandardDeviation {
     private static double StandardDeviation1(int[] numbers) {
         var total = 0.0;
         var count = 0;
-        foreach (var number in numbers) {
+        foreach (var number in numbers) { // loops goes through each # in the array - each iteration does constant time work adding to total and increasing count so the complexity is O(n) 
             total += number;
             count += 1;
         }
 
-        var avg = total / count;
+        var avg = total / count; //single division operationg, it doesn't depend on the size of the input, making it O(1)
         var sumSquaredDifferences = 0.0;
-        foreach (var number in numbers) {
+        foreach (var number in numbers) { //this loop goes through each number in the array  like the other line - calculates the ^ difference and adding it to sumQuaredDifferences, making it O(n)
             sumSquaredDifferences += Math.Pow(number - avg, 2);
         }
 
-        var variance = sumSquaredDifferences / count;
+        var variance = sumSquaredDifferences / count; //div and square root calculation, both are constant time, making it O(1)
         return Math.Sqrt(variance);
     }
 
-    private static double StandardDeviation2(int[] numbers) {
-        var sumSquaredDifferences = 0.0;
+    private static double StandardDeviation2(int[] numbers) { //takes the array of integers(numbers) and returns them double
+        var sumSquaredDifferences = 0.0;//adds thes squared difference to count how many #s are processed
         var countNumbers = 0;
         foreach (var number in numbers) {
             var total = 0;
@@ -41,24 +41,24 @@ public static class StandardDeviation {
                 count += 1;
             }
 
-            var avg = total / count;
-            sumSquaredDifferences += Math.Pow(number - avg, 2);
-            countNumbers += 1;
+            var avg = total / count; // constant time, O(1)
+            sumSquaredDifferences += Math.Pow(number - avg, 2); //constant time, O(1)
+            countNumbers += 1; //constant time, O(1)
         }
 
-        var variance = sumSquaredDifferences / countNumbers;
-        return Math.Sqrt(variance);
-    }
+        var variance = sumSquaredDifferences / countNumbers; //O(1)
+        return Math.Sqrt(variance); //O(1)
+    }//O(n) x  0(n) = O(n^2)
 
     private static double StandardDeviation3(int[] numbers) {
-        var count = numbers.Length;
-        var avg = (double)numbers.Sum() / count;
-        var sumSquaredDifferences = 0.0;
-        foreach (var number in numbers) {
+        var count = numbers.Length; //O(1)
+        var avg = (double)numbers.Sum() / count;//O(n)
+        var sumSquaredDifferences = 0.0;//O(1)
+        foreach (var number in numbers) {//O(n)
             sumSquaredDifferences += Math.Pow(number - avg, 2);
         }
 
-        var variance = sumSquaredDifferences / count;
-        return Math.Sqrt(variance);
+        var variance = sumSquaredDifferences / count;//O(1)
+        return Math.Sqrt(variance);//O(1)
     }
-}
+}//O(1)+O(n)+O(n)+O(1)+O(1) = O(2n+3) = O(n)
