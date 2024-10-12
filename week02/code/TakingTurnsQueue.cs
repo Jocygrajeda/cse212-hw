11049ccc074240a32b9cb7d5fa3253f52eea17c5
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2024-10-11 23:00:17 -0500

    more comments

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -7,21 +7,21 @@
 /// less than they will stay in the queue forever.  If a person is out of turns then they will 
 /// not be added back into the queue.
 /// </summary>
-public class TakingTurnsQueue
+public class TakingTurnsQueue //declares the class ttq
 {
-    private readonly PersonQueue _people = new();
+    private readonly PersonQueue _people = new();//initialized a private field(_people) of type PersonQueue, queue for managing persons objects.
 
-    public int Length => _people.Length;
+    public int Length => _people.Length;//lenght returns the current length of the _people queue. => creates a property that returns value
 
     /// <summary>
     /// Add new people to the queue with a name and number of turns
     /// </summary>
     /// <param name="name">Name of the person</param>
     /// <param name="turns">Number of turns remaining</param>
-    public void AddPerson(string name, int turns)
+    public void AddPerson(string name, int turns)//AddPerson takes a name and turns
     {
-        var person = new Person(name, turns);
-        _people.Enqueue(person);
+        var person = new Person(name, turns);//creates a new person object with the proved name and turns
+        _people.Enqueue(person);//adds the created person to the back of the _people queue using the enqueue method
     }
 
     /// <summary>
@@ -31,26 +31,26 @@ public class TakingTurnsQueue
     /// person has an infinite number of turns.  An error exception is thrown 
     /// if the queue is empty.
     /// </summary>
-    public Person GetNextPerson()
+    public Person GetNextPerson()//GetNextPerson returns a Person
     {
-        if (_people.IsEmpty())
+        if (_people.IsEmpty())//checks if the _people queue is empty.
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
+        else//executes if the queue is not empty
         {
-            Person person = _people.Dequeue();
+            Person person = _people.Dequeue();//removes and retrieves the next person from the front of _people
             if (person.Turns > 1)
             {
                 person.Turns -= 1;
                 _people.Enqueue(person);
             }
 
-            return person;
+            return person;//checks if the person has more thanone turn left
         }
     }
 
-    public override string ToString()
+    public override string ToString()//overrides the ToString method to return _people queue
     {
         return _people.ToString();
     }
